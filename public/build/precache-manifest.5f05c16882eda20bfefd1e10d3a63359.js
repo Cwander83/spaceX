bbc2fe680f59355eca30b4ec9b6e72cf1008fbe9
Chris Wander <31929558+Cwander83@users.noreply.github.com>
2019-12-16 19:55:17 -0500

    folder change

---
+++ public/build/precache-manifest.5f05c16882eda20bfefd1e10d3a63359.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e593cb52445ce0dd6da",
+    "url": "/static/css/main.7d2426c9.chunk.css"
+  },
+  {
+    "revision": "0e593cb52445ce0dd6da",
+    "url": "/static/js/main.7b5e1637.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "344bdfaefcc8b7588f90",
+    "url": "/static/js/2.7d39f655.chunk.js"
+  },
+  {
+    "revision": "c83ca211fbedcdc79b0e75fc7cc7fb4f",
+    "url": "/index.html"
+  }
+];
